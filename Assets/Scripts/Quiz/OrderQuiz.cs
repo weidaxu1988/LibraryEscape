a152593dd9214e8702368ee2d0dc4c3a95bfede3
weidaxu1988 <wdxu88@gmail.com>
2015-03-26 23:36:31 +0800

    player update

---
+++ Assets/Scripts/Quiz/OrderQuiz.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrderQuiz : Quiz
+{
+    List<OrderQuizItem> questionList = new List<OrderQuizItem>();
+
+    void Awake()
+    {
+        questionList.AddRange(GetComponentsInChildren<OrderQuizItem>());
+    }
+
+
+
+    public override int getScore()
+    {
+        int total = questionList.Count;
+        int score = 0;
+        foreach (OrderQuizItem c in questionList)
+        {
+            if (c.getResult())
+                score++;
+        }
+        return score / total;
+    }
+}
---
+++ Assets/Scripts/Quiz/OrderQuizItem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrderQuizItem : MonoBehaviour
+{
+    public int order;
+
+    private UIInput input;
+
+    void Awake()
+    {
+        input = GetComponentInChildren<UIInput>();
+    }
+
+    public void OnOrderInput()
+    {
+        string str = input.value;
+        char c = str[0];
+
+        if (str.Length > 1)
+        {
+            input.value = c.ToString();
+        }
+
+        if (c != '1' && c != '2' && c != '3' && c != '4' && c != '5' && c != '6' && c != '7' && c != '8' && c != '9')
+        {
+            input.value = "";
+        }
+    }
+
+    public bool getResult()
+    {
+        bool result = false;
+        string content = input.value;
+        if (string.IsNullOrEmpty(content))
+        {
+            int ord = int.Parse(content);
+            if (ord == order)
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
