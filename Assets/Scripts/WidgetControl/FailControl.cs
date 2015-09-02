cb1f6c8ee7f08dc4c5959430101f9cfa10911135
weidaxu1988 <wdxu88@gmail.com>
2015-09-02 21:08:28 +0800

    commit

--- Assets/Scripts/WidgetControl/FailControl.cs
+++ Assets/Scripts/WidgetControl/FailControl.cs
@@ -1,7 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FailControl : MonoBehaviour {
+public class FailControl : MonoBehaviour
+{
+    private Animator libraianAnimator;
+
+    void OnEnable()
+    {
+        if (libraianAnimator == null)
+        {
+            libraianAnimator = GetComponentInChildren<Animator>();
+        }
+        if (libraianAnimator != null)
+        {
+            libraianAnimator.SetTrigger("concerned");
+        }
+    }
+
     public void LoadSelectScene()
     {
         GameManager.instance.SendEmail("type: level, failed level, index: " + LevelControl.instance.levelIndex);
